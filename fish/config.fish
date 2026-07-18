c93ffe4a46e6266538dc98a93a801cd6d6d7caac
vvs0x <vschwarz@ik.me>
2026-07-18 20:10:40 +0200

    got nushell and deleted fish cause it's trashhhhhhhh

--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-fzf --fish | source
-zoxide init fish | source
-
-set -g __fish_git_prompt_showdirtystate 1
-set -g __fish_git_prompt_showuntrackedfiles 1
-set -g __fish_git_prompt_char_dirtystate '*'
-set -g __fish_git_prompt_char_untrackedfiles '?'
-set -g fish_autosuggestion_enabled 1
-set -gx EDITOR nvim
